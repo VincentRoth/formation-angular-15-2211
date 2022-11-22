ca900ab0758428a82e7b32a675700e9bdb70b53c
Vincent Roth <vincent@placeme.io>
2022-11-22 14:04:43 +0100

    Module et composant

---
+++ src/app/animal/animal-details/animal-details.component.ts
@@ -0,0 +1,20 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Animal } from '../../shared/api/animal';
+
+@Component({
+  selector: 'app-animal-details',
+  templateUrl: './animal-details.component.html',
+  styleUrls: ['./animal-details.component.scss'],
+})
+export class AnimalDetailsComponent implements OnInit {
+  animal: Animal;
+
+  ngOnInit(): void {
+    this.animal = {
+      name: 'Mon animal',
+      species: 'chat',
+      veterinarian: 'Panoramix',
+    };
+  }
+}
