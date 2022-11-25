e26db3f964710c6e6bbaf5c464341569ec74cf16
Vincent Roth <vincent@placeme.io>
2022-11-25 09:37:39 +0100

    Environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'veterinarian2211bis';
+
+  getEnv(): string {
+    return environment.production ? 'PROD' : 'DEV';
+  }
 }
